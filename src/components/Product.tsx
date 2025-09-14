89a3a57c55a27797af641124359064a3bcab8b18
joaobiankati <joaobiankati0164@email.com>
2025-09-14 01:04:19 -0300

    Criei um componente Product com toggle de finalização e botão de remover

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,5 @@
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+
 interface Props {
     name: string;
     finalized: boolean;
@@ -5,6 +7,97 @@ interface Props {
     onToggle: () => void;
 }
 
-export function Product({} : Props) {
-    return 
+export function Product({name, finalized = false, onRemove, onToggle} : Props) {
+    return (
+        <View style={[styles.container, finalized && styles.containerChecked]}>
+            <TouchableOpacity onPress={onToggle} activeOpacity={0.1}>
+                {finalized ? (
+                    <View style={styles.checkButtonPurple}>
+                        <Text style={styles.checkmark}>✓</Text>
+                    </View>
+                ) : (
+                    <View style={styles.checkButton}>
+                    </View>
+                )}
+            </TouchableOpacity>
+            
+            <Text style={[styles.productName, finalized && styles.productNameChecked]}>
+                {name}
+            </Text>
+            
+            <TouchableOpacity onPress={onRemove} style={styles.removeButton} activeOpacity={0.1}>
+                <Image 
+                    source={require("../../assets/trash.png")}
+                    style={styles.removeIcon}
+                    resizeMode="contain"
+                />
+            </TouchableOpacity>
+        </View>
+    )
 }
+
+const styles=StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        padding: 12,
+        height: 56,
+        alignItems: "center",
+        borderRadius: 8,
+        borderWidth: 1,
+        backgroundColor: "#F2F2F2",
+        shadowColor: "#000000",
+        shadowOpacity: 0.1,
+        shadowRadius: 8,
+        shadowOffset: {height: 2, width: 0},
+        justifyContent: "space-between",
+        borderColor: "#808080",
+        marginBottom: 8,
+    },
+
+    containerChecked: {
+        flexDirection: "row",
+        padding: 12,
+        height: 56,
+        alignItems: "center",
+        borderRadius: 8,
+        borderWidth: 1,
+        backgroundColor: "#F2F2F2",
+        justifyContent: "space-between",
+        borderColor: "#D9D9D9",
+        marginBottom: 8,
+    },
+
+    checkButton: {
+        
+    },
+
+    checkButtonPurple: {
+        
+    },
+
+    productName: {
+        
+    },
+
+    productNameChecked: {
+        
+    },
+
+    removeButton: {
+        
+    },
+
+    removeIcon: {
+        
+    },
+
+    checkmark: {
+        
+    },
+
+    emptyCheck: {
+        
+    }
+
+
+})
